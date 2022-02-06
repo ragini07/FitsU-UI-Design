cb08820437e769ff86d5d4d3ebac1a8ebf23b3b4
ragini07 <40401373+ragini07@users.noreply.github.com>
2022-02-07 02:38:04 +0530

    naming

--- Alter/alert.js
+++ Alter/alert.js
@@ -11,7 +11,7 @@ const showAlertBtn = document.querySelector(".show-alert-btn");
 const divToShowAlert = document.querySelector(".live")
 showAlertBtn.addEventListener('click',() => {
     var div = document.createElement('div');
-    div.classList.add('alter');
+    div.classList.add('alert');
     div.classList.add('success-lg');
     div.innerHTML= `<i class="fas fa-check-circle"></i><strong>Success !</strong><p>Successful or positive action</p> 
     <i class="fas fa-times dismiss"></i>`;
