90ff3ab1757496e00a8edadf9e6870e7137fab3c
ragini07 <40401373+ragini07@users.noreply.github.com>
2022-02-20 20:20:37 +0530

    font size fix

--- Home Page/main.js
+++ Home Page/main.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector(".menu-btn");
 const sideBar = document.querySelector(".side-content-mobile");
 const mainContent = document.querySelector(".main-container");
 
-
+//mobile side bar
 hamburger.addEventListener('click',() => {
     sideBar.classList.add("show")
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
@@ -12,3 +12,5 @@ mainContent.addEventListener('click',() =>{
     sideBar.classList.remove("show")
     document.body.style.backgroundColor = "rgb(241, 241, 241)";
 })
+
+//theme
