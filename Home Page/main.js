5ef90808df8b57c75b884bbc4d4d413d74f786f2
ragini07 <40401373+ragini07@users.noreply.github.com>
2022-02-22 00:40:43 +0530

    feat : dark mode

--- Home Page/main.js
+++ Home Page/main.js
@@ -2,16 +2,53 @@ const hamburger = document.querySelector(".menu-btn");
 const sideBar = document.querySelector(".side-content-mobile");
 const mainContent = document.querySelector(".main-container");
 
+const themeIcon = document.querySelector(".theme");
+
 //mobile side bar
 hamburger.addEventListener('click',() => {
     sideBar.classList.add("show")
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 })
 
 mainContent.addEventListener('click',() =>{
     sideBar.classList.remove("show")
-    document.body.style.backgroundColor = "rgb(241, 241, 241)";
 })
 
 
+//theme
+
+const darkMode = () => {
+        //icon toggle
+        themeIcon.childNodes[0].classList.remove('fa-moon')
+        themeIcon.childNodes[0].classList.add('fa-sun')
+
+        //theme set
+        document.documentElement.setAttribute('data-theme', 'dark');
+}
+const lightMode = () => {
+        themeIcon.childNodes[0].classList.add('fa-moon')
+        themeIcon.childNodes[0].classList.remove('fa-sun')
+
+        document.documentElement.setAttribute('data-theme', 'light');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const currenetTheme = localStorage.getItem('theme')
+	if (currenetTheme === 'light') {
+		lightMode()
+	} else {
+        darkMode()
+	}
+});
+
+themeIcon.addEventListener('click' , () => {
+    if(themeIcon.childNodes[0].classList.contains('fa-moon')){
+        darkMode()
+        localStorage.setItem('theme', 'dark');
+    }
+    else{  
+        lightMode()
+        localStorage.setItem('theme', 'light');
+    }
+})
 
